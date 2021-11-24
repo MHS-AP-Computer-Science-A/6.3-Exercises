28a526e19406b160cb3308415b66bedf9edb8df5
foxrw1 <78601276+foxrw1@users.noreply.github.com>
2021-11-24 11:27:20 -0500

    Add files via upload

---
+++ src/Ex6_3_1_ModifyLinearSearch.java
@@ -0,0 +1,50 @@
+
+public class Ex6_3_1_ModifyLinearSearch {
+	public static void main(String[] args) {
+		String[] words = { "Lions", "Wolverines", "Tigers", "Loons", "Spartans", "Cardinals", "Chips" };
+
+		System.out.println("Testing linearSearch1:");
+		linearSearch1(words, "Loons");
+
+		System.out.println("\nTesting linearSearch1 not found:");
+		linearSearch1(words, "Bears");
+
+		System.out.println("\nTesting linearSearch2:");
+		linearSearch2(words, "Chips");
+
+		System.out.println("\nTesting linearSearch2 not found:");
+		linearSearch2(words, "Hoosiers");
+	}
+
+	// Modify this to search for a String in a String array
+	static void linearSearch1(int[] arr, int toFind) {
+		boolean isFound = false;
+
+		for (int i = 0; i < arr.length; i++) {
+			if (toFind == arr[i]) {
+				isFound = true;
+				System.out.println(toFind + " found at position " + i);
+			}
+		}
+
+		if (!isFound) {
+			System.out.println(toFind + " was not found.");
+		}
+	}
+
+	// Modify this to search for a String in a String array
+	static void linearSearch2(int[] arr, int toFind) {
+		boolean isFound = false;
+
+		for (int item : arr) {
+			if (toFind == item) {
+				isFound = true;
+				System.out.println(toFind + " found!");
+			}
+		}
+
+		if (!isFound) {
+			System.out.println(toFind + " was not found.");
+		}
+	}
+}
